099862bcba9b47877e46356bb6f8c6d8352732a2
Attula15 <herincs.bence.kdg@gmail.com>
2023-06-01 16:59:19 +0200

    itt maradt

--- UnitTests/commands/CollectTest.java
+++ UnitTests/commands/CollectTest.java
@@ -1,12 +1,80 @@
 package commands;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import src.Board;
+import src.Equipment;
+import src.Virologist;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CollectTest {
 
+    Create create;
+    Board board;
+    Collect collect;
+
+    @BeforeEach
+    void initEach(){
+        System.out.println("A test has begun");
+        board = new Board();
+        System.out.println("A board has been created");
+        create = new Create();
+        System.out.println("A create has been created");
+        collect = new Collect();
+        System.out.println("A collect has been created");
+    }
+
     @Test
-    void collect() {
+    //Sikeresen felvesz egy genetical kodot a virologus egy laborbol
+    void collectEquipmentSuccess() {
+        //Letrehozom a equipment-et
+        String[] createArgs = {"create", "equipment", "cape", "0"};
+        create.create(createArgs, board);
+        //A sheltert
+        createArgs = new String[]{"create","shelter","cape0"};
+        create.create(createArgs, board);
+        //A virologus letetele a mezore
+        createArgs = new String[]{"create", "virologist", "0", "0", "0.1", "shelter0"};
+        create.create(createArgs, board);
+
+        //Virologus kiszedese
+        Virologist v1 = board.getVirologusok().get(0);
+
+        //Felveszi az equipmentet
+        String[] collectArgs = {"collect", "virologist0"};
+        collect.collect(collectArgs, board);
+
+        //A megfelelo equipmentnek nala kell lennie
+        assertEquals(board.getFelszerelesek().get(0) ,v1.getInv().GetEquipments().get(0));
+    }
+
+    @Test
+    //Egy olyan mezon szeretne collectelni, ahol nincs mit
+    void collectEquipmentFailure() {
+        String[] createArgs = {"create", "equipment", "cape", "0"};
+        create.create(createArgs, board);
+        createArgs = new String[]{"create","shelter", "cape0"};
+        create.create(createArgs, board);
+        createArgs = new String[]{"create", "field", "shelter0"};
+        create.create(createArgs, board);
+
+        //A shelterrel szomszedos ures mezore teszem le a virologust
+        createArgs = new String[]{"create", "virologist", "0", "0", "0.1", "field1"};
+        create.create(createArgs, board);
+
+        //Virologus kiszedese
+        Virologist v1 = board.getVirologusok().get(0);
+        //Egy ures equipment listat letrehozok, hogy lehessen mihez hasonlitani
+        ArrayList<Equipment> emptyEquipmentArrayList = new ArrayList<>();
+
+        //Megprobalja felvenni az equipmentet
+        String[] collectArgs = {"collect", "virologist0"};
+        collect.collect(collectArgs, board);
+
+        //Uresnek kell lennie a felszerelesek listajanak
+        assertEquals(emptyEquipmentArrayList ,v1.getInv().GetEquipments());
     }
 }
--- UnitTests/commands/MoveTest.java
+++ UnitTests/commands/MoveTest.java
@@ -1,11 +1,73 @@
 package commands;
 
+import org.junit.After;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import src.Board;
+import src.Virologist;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class MoveTest {
 
+    Board board;
+    Create create;
+    Move move;
+
+    @BeforeEach
+    void initEach(){
+        System.out.println("A test has begun");
+        board = new Board();
+        System.out.println("A board has been created");
+        create = new Create();
+        System.out.println("A create has been created");
+        move = new Move();
+        System.out.println("A move has been created");
+    }
+
     @Test
-    void move() {
+    //Alap mozgas teszt ket field kozott
+    void moveSuccess() {
+        //2 szomszedos mezo letrehozasa
+        String[] createArgs = {"create", "field"};
+        create.create(createArgs, board);
+        createArgs = new String[]{"create", "field", "field0"}; //Szomszedosak egymassal
+        create.create(createArgs, board);
+
+        //Virologus letrehozasa
+        createArgs = new String[]{"create", "virologist", "0", "0", "0.1", "field0"};   //Letrehozok egy virologust, aki a field1-an kezd
+        create.create(createArgs, board);
+        Virologist v1 = board.getVirologusok().get(0);
+
+        //A move tenylegese tesztelese
+        String[] moveArgs = {"move", "Virologist0", "field1"};
+        move.move(moveArgs, board);
+
+        //Az uj mezore lepett-e a virologus
+        assertEquals(board.getMezok().get(1), v1.getField());
+    }
+
+    @Test
+    void moveFailure(){
+        //Ezzel letrehozok 3 fieldet egy sorba kotve
+        String[] createArgs = {"create","field"};
+        create.create(createArgs, board);
+        createArgs = new String[]{"create", "field", "field0"};
+        create.create(createArgs, board);
+        createArgs = new String[]{"create", "field", "field1"};
+        create.create(createArgs, board);
+
+        //Virologus letrehozasa
+        createArgs = new String[]{"create", "virologist", "0", "0", "0.1", "field0"};   //Letrehozok egy virologust, aki a field1-an kezd
+        create.create(createArgs, board);
+        Virologist v1 = board.getVirologusok().get(0);
+
+        //Mozgas egy olyan mezore, amire nem lehetseges
+        String[] moveArgs = {"move", "Virologist0", "field2"};
+        move.move(moveArgs, board);
+
+        //Nem szabadott atlepnie, azaz meg mindig a kezdo mezon kell allnia
+        assertEquals(board.getMezok().get(0), v1.getField());
     }
 }
